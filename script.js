1285779be145ebf93c9cf28a3a82c5fbae96c591
goofy9901 <borissavicc789@gmail.com>
2026-03-28 13:57:22 +0100

    Finished Calculator

--- script.js
+++ script.js
@@ -19,13 +19,51 @@ btnCont.classList = "btnCont";
 
 let btnText = ["C", "/", "*", "-", 7, 8, 9, "+", 4, 5, 6, "=", 1, 2, 3, 0];
 
+const clearDisplay = function () {
+  display.textContent = "";
+  number1 = "";
+  number2 = "";
+  operator = "";
+  result = "";
+};
+
+let number1 = "";
+let number2 = "";
+let result = "";
+let operator = "";
+
 for (let i = 0; i < btnText.length; i++) {
   const cell = document.createElement("div");
   btnCont.appendChild(cell);
   cell.classList = "cell";
   cell.textContent = btnText[i];
   cell.addEventListener("click", function () {
-    display.textContent += cell.textContent;
+    if (
+      cell.textContent === "+" ||
+      cell.textContent === "-" ||
+      cell.textContent === "*" ||
+      cell.textContent === "/"
+    ) {
+      if (operator === "") {
+        number1 = display.textContent;
+      } else {
+      }
+
+      operator = cell.textContent;
+      display.textContent += operator;
+    } else if (cell.textContent === "=") {
+      if (!operator) return;
+      number2 = display.textContent.split(operator)[1];
+      if (number2 === undefined || number2 === "") return;
+      operate();
+      display.textContent = result;
+      number1 = result;
+      operator = "";
+    } else if (cell.textContent === "C") {
+      clearDisplay();
+    } else {
+      display.textContent += cell.textContent;
+    }
   });
 }
 
@@ -40,15 +78,15 @@ const multiply = function (a, b) {
 };
 
 const divide = function (a, b) {
+  if (b === 0) {
+    return "Error";
+  }
   return a / b;
 };
 
-let number1 = "";
-let number2 = "";
-let result = "";
-let operator = "";
-
 const operate = function () {
+  number1 = Number(number1);
+  number2 = Number(number2);
   switch (operator) {
     case "+":
       result = add(number1, number2);
